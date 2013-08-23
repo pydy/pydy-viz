eddaca387b6c95df82e22ad9df3de339bcb6d584
Tarun Gaba <Tarun Gaba>
2013-08-24 02:03:52 +0530

    WIP

--- pydy_viz/static/js/src/canvas.js
+++ pydy_viz/static/js/src/canvas.js
@@ -3,6 +3,11 @@ var Canvas = function(JSONObj)
     var parent = this; // Stay away from this ..
     parent.frames = JSONObj.frames;
     parent.height = JSONObj.height;
+    parent.axes = new THREE.Object3D();
+    parent.mesh_dict = {};
+    
+    // Initialize method ..
+    
     
     
     }
