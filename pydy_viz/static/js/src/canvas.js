45e34a2f9fe5c91ff0fbbab9bdec5b83755c616a
Tarun Gaba <Tarun Gaba>
2013-08-23 23:48:21 +0530

    WIP

--- pydy_viz/static/js/src/canvas.js
+++ pydy_viz/static/js/src/canvas.js
@@ -1,1 +1,11 @@
-// Sample canvas.js file for socket connection testing
+var Canvas = function(JSONObj)
+    {
+    var parent = this; // Stay away from this ..
+    parent.frames = JSONObj.frames;
+    parent.height = JSONObj.height;
+    
+    
+    }
+    
+        
+
